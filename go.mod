20394d4a4fcc7226e14ce3957187394bb93b529a
Alexey Semigradsky <alexey.semigradsky@gmail.com>
2020-12-13 08:03:43 +0300

    upgrade hkmh to 1.0.0 and add one CLI argument

--- go.mod
+++ go.mod
@@ -3,8 +3,7 @@ module github.com/AlexeySemigradsky/hkow
 go 1.15
 
 require (
-	github.com/AlexeySemigradsky/hkmh v0.0.0-20201212194643-3de265eed1b2
-	github.com/AlexeySemigradsky/mh v0.0.0-20201212174118-a31258b88565 // indirect
+	github.com/AlexeySemigradsky/hkmh v1.0.0
 	github.com/brutella/hc v1.2.3
 	github.com/xiam/to v0.0.0-20200126224905-d60d31e03561 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
--- hkow.go
+++ hkow.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
+	"time"
 
 	"github.com/AlexeySemigradsky/hkmh"
 
@@ -10,11 +13,24 @@ import (
 )
 
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("usage: hkow /path/to/storage/")
+		return
+	}
+
+	storagePath := os.Args[1]
+	stat, err := os.Stat(storagePath)
+	if err != nil || !stat.Mode().IsDir() {
+		fmt.Println("usage: hkow /path/to/storage/")
+		return
+	}
+
 	bridge := accessory.NewBridge(accessory.Info{Name: "Bridge", ID: 1})
 
 	deskLight, err := hkmh.NewAccessory(
 		accessory.Info{Name: "Desk Light", ID: 2},
 		"192.168.1.211:5577",
+		3*time.Second,
 	)
 
 	if err != nil {
@@ -24,13 +40,17 @@ func main() {
 	bedLight, err := hkmh.NewAccessory(
 		accessory.Info{Name: "Bed Light", ID: 3},
 		"192.168.1.216:5577",
+		3*time.Second,
 	)
 
 	if err != nil {
 		log.Panicln(err)
 	}
 
-	config := hc.Config{Pin: "00207700"}
+	config := hc.Config{
+		StoragePath: storagePath,
+		Pin:         "00207700",
+	}
 	t, err := hc.NewIPTransport(
 		config,
 		bridge.Accessory,
